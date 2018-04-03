4bbee34967c8807ff46c45065c706ac8bed38044
Ashish Lepcha <lepchahalpern@gmail.com>
2018-04-04 00:18:35 +0530

    Update pri.c

--- pri.c
+++ pri.c
@@ -3,6 +3,28 @@
  
 int main()
 {
+    int n, cpuUsage[20],i,j,pit[20];
+    printf("Enter total number of processes:");
+    scanf("%d", &n);
  
+    printf("\nEnter new Cpu Usage\n");
+    for(i=0;i<n;i++)
+    {
+        printf("p[%d]:",i+1);
+        scanf("%d", &cpuUsage[i]);
+    }
+    pit[0]=0;
+    //calculating new priorities;
+    for(i=0;i<n;i++)
+    {
+            pit[i]=((cpuUsage[i] / 2)+ 60);
+            
+            
+    }
+ 
+    printf("\nProcess\t\t\t\tNewPriority");
+    for(i=0;i<n;i++){
+    printf("\np[%d]\t\t\t\t%d\n",i+1,pit[i]);
+ }
     return 0;
 }
